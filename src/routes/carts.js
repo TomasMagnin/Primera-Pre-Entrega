48ad160490916b92003548d7d8a713a766abcafe
TomasMagnin <tomasmagnin@hotmail.com>
2023-05-11 22:05:29 -0300

    Primera entrega P Final v2

--- src/routes/carts.js
+++ src/routes/carts.js
@@ -20,3 +20,37 @@ cartsRouter.get("/:cid", async (req, res) => {
     idFound ?  res.status(200).send(idFound) : res.status(404).send('Not Found');
 
 });
+
+
+cartsRouter.post("/:cid/product/:pid", async (req,res) => {
+    let productsCart = req.body;
+    let { cid } = req.params;
+    cid = parseInt(cid);
+    let { pid } = req.params;
+    pid = parseInt(pid);
+    const instanceManager = new ProductManager("./src/carts.json");     // Instanciamos una clase, pasando como parametro la ruta que tiene el archivo carts.json con los carritos de compra.
+    const viewCart = await instanceManager.getProducts();
+    const cartFound = viewCart.find(item => item.id == cid);            // Bucamos el carrito, por el id que recivimos por params, usando la funcion find cuando encuentre la igualdad.
+    if(cartFound) {
+        const productFound = cartFound.products.find(item => item.product == pid);  // Buscamos si se encuentra el ID de producto, dentro del carrito.
+        let newProductCart = [];                                                    // Creamo un array, para los productos del carrito.
+        if(productFound){                                                           // Preguntamos si ya existia ese producto en el carrito.        
+            let newQuantity = productsCart.quantity + productFound.quantity         // Creamos la variable nueva cantidad y sumamos la cantidad del producto que ya estaba mas la del nuevo producto.
+            newProductCart.push(...cartFound.products);                             // Desparramamos las propiedades del carrito encontrado en el array.
+            newProductCart.map(item => {                                            // Creamos un nuevo array a partir de la funcion que le pasamos.
+                if(element.product == productsCart.product){
+                    element.quantity = newQuantity;
+                }
+            });
+        }
+        else {
+            newProductCart.push(...cartFound.products);                             // Desparramamos las propiedades del producto para insertarla en el array.
+            newProductCart.push( productsCart );                                    // Introducimos las propiedades enviadas en el body
+        }
+        const flagFound = await instanceManager.updateProduct(cid, { "products" : newProductCart});
+        flagFound ? res.status(200).send("Product added/modified succesfullly") : res.status(404).send("not Found");
+    } 
+    else{
+        res.status(404).send("Not Found");
+    }
+});
